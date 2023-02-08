44e8bdca538891ba650151a043ea613ffc6ea290
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-02-08 19:24:54 +0000

    Updated script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ getAPI()
 .then(data => {
     console.log(data);
     globalData = data;
-    loadTableData(data);
+    loadTableData(globalData);
 })
 .catch(err => console.log('rejected', err.message));
 
@@ -25,23 +25,41 @@ getAPI()
     let sortDirection = false;    
 
     
-    function loadTableData(data) {
+    function loadTableData(globalData) {
         const tableBody = document.getElementById('tableData');
         let dataHtml = '';
     
-        for (let interaction of data) {
+        for (let interaction of globalData) {
             dataHtml += `<tr><td>${interaction.date}</td><td>${interaction.name}</td><td>${interaction.sector_id}</td></tr>`;
         }
         tableBody.innerHTML = dataHtml;
 
     }
 
+    //show the percentages of interactions this client has for each sector.
+
+    function sortStringColumn(sort, columnName) {
+        globalData = globalData.sort((p1,p2) => {
+            return sort ? p1[columnName] - p2[columnName] : p2[columnName] - p1[columnName]
+        });
+    }
     
     function sortColumn(columnName) {
-        const dataType = globalData[0][columnName];
-        console.log(dataType);
+        const dataType = typeof globalData[0][columnName];
         sortDirection = !sortDirection; 
+        
+
+        switch(dataType) {
+            case 'string':
+                sortStringColumn(sortDirection, columnName);
+                break;
+        }
+
+        loadTableData(globalData);
+
     }
+   
+ 
 
 
 
